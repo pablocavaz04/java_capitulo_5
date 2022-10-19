8121cedea419725c7f82537c3f90e202a2667c80
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-19 11:59:08 +0200

    First Commit

--- Cap5Ejercicio04.java
+++ Cap5Ejercicio04.java
@@ -1,7 +1,11 @@
 /*
- * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizandoun bucle for.
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle for.
  *Author:Pablo Camino Vázquez
  */
 public class Cap5Ejercicio04 {
-    
+    public static void main(String[] args) {
+        for (int i = 320 ; i>=160; i-=20){
+        System.out.println(i);
+        }
+    }
 }
--- Cap5Ejercicio05.java
+++ Cap5Ejercicio05.java
@@ -1,7 +1,13 @@
 /*
- * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizandoun bucle while.
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle while.
  * Author:Pablo Camino Vázquez
  */
 public class Cap5Ejercicio05 {
-    
+    public static void main(String[] args) {
+        int i = 320 ;
+        while (i >=160 ){
+            System.out.println(i);
+            i-=20;
+        }
+    }
 }
