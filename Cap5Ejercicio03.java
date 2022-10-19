d1be43b50d521164ca1778b39b8ff748bb3a6fc2
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-19 10:48:29 +0200

    First Commit

---
+++ Cap5Ejercicio03.java
@@ -0,0 +1,7 @@
+/*
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
+ * Author:Pablo Camino Vázquez
+ */
+public class Cap5Ejercicio03 {
+    
+}
---
+++ Cap5Ejercicio04.java
@@ -0,0 +1,7 @@
+/*
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizandoun bucle for.
+ *Author:Pablo Camino Vázquez
+ */
+public class Cap5Ejercicio04 {
+    
+}
