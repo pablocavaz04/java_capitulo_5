45e2fe8a17e4fb9d86359352d9a0d20bc83c6f59
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-07 09:41:31 +0100

    First Commit

--- Cap5Ejercicio22.java
+++ Cap5Ejercicio22.java
@@ -1,3 +1,23 @@
+/**
+ * Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos.
+ * 
+ * Author: Pablo Camino Vázquez
+ * 
+ */
+import java.util.Scanner;
+
+import javax.swing.text.AbstractDocument.Content;
 public class Cap5Ejercicio22 {
-    
+    public static void main(String[] args) {
+        for ( int i= 2; i<=100;i++){
+            boolean esprimo=true;
+            for(int contador=2;contador<=i/2;contador++){
+                if(i%contador == 0){
+                    esprimo=false;
+                }
+            }
+            if(esprimo)
+            System.out.println(i);
+        }
+    }
 }
